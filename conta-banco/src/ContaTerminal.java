51710ff352722ebbf2bc7e4af88168df334e295a
Math <matheus-deluna@hotmail.com>
2023-10-10 11:27:25 -0300

    meu projeto de conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá Mundo");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("----------------------------");
+        System.out.println("BANCO DIGITAL INNOVATION ONE");
+        System.out.println("----------------------------");
+
+        System.out.print("Por favor, digite seu nome: ");
+        String nome = scanner.next();
+        System.out.print("Digite o número da agencia: ");
+        String agencia = scanner.next();
+        System.out.print("Digite o número da conta: ");
+        int conta = scanner.nextInt();
+        System.out.print("Digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println();
+        System.out.print("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia);
+        System.out.println(", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
